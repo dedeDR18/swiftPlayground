40da085cc5a1bb952b10ee91ea26d982ab945aa3
dbesar <dededarirahmadi@gmail.com>
2022-06-15 00:10:01 +0700

    Initial Commit

---
+++ MySwiftPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+var greeting = "Hello, "
+
+var myName = "Dede Dari Rahmadi"
+
+var string = "namaku adalah \(myName)"
+
+var str = """
+Swift
+I love Swift
+hahahaha
+"""
+
+// print with terminator space
+/*print("Good Morning!")
+ asda
+ */
+print("It's rainy today")
+
+//print(str)
